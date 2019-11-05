f3324e0d79792288bfa6b934c2c93659179d0d89
Valentin Robert <valentin.robert.42@gmail.com>
2019-11-05 10:53:36 -0800

    update README, add dependency

--- lib/ExposedModule.hs
+++ lib/ExposedModule.hs
@@ -1,8 +1,11 @@
 module ExposedModule where
 
-import Data.List (or)
+import Data.List          ( or )
+import Data.Time.Calendar ( Day )
 
 import OtherModule
+
+type MyDay = Day
 
 c :: Bool
 c = or [a, b, a && b, a || b]
